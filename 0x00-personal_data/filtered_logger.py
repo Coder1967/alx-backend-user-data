4957dae57078a1068f6f0f4b66a81e23b00c6940
Nosa <nosakhareaghedo42@gmail.com>
2023-03-02 21:06:34 +0000

    completed another task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,32 @@
 filterd logger module
 """
 import re
-from typing import Tuple
+from typing import Tuple, List
+import logging
 
 
-def assist_filter(fields, redaction, separator) -> Tuple:
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(self.fields, self.REDACTION,
+                                message, self.SEPARATOR)
+        return redacted
+        
+
+def assist_filter(fields: List[str], redaction: str,
+                  separator: str) -> Tuple:
     """
     assists filter_datum function
     """
@@ -16,8 +38,9 @@ def assist_filter(fields, redaction, separator) -> Tuple:
     return patterns, replace
 
 
-def filter_datum(fields, redaction,
-                 message, separator) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
     patterns, repl = assist_filter(fields, redaction, separator)
     for i in range(len(fields)):
         message = re.sub(patterns[i], repl[i], message)
