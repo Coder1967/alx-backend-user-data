a3f1903b133357c25487a82104bc77708e6cb509
Nosa <nosakhareaghedo42@gmail.com>
2023-03-02 20:49:01 +0000

    completing first task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,17 +5,19 @@ filterd logger module
 import re
 from typing import Tuple
 
-def assist_filter(fields, redaction, separator)->Tuple:
+
+def assist_filter(fields, redaction, separator) -> Tuple:
     """
     assists filter_datum function
     """
-    patterns = [r"(?P<field>{})=[^{}]*".format(field, separator) for field in fields]
+    patterns = [r"(?P<field>{})=[^{}]*".format(field,
+                separator)for field in fields]
     replace = ["{}={}".format(field, redaction) for field in fields]
     return patterns, replace
 
 
 def filter_datum(fields, redaction,
-        message, separator)->str:
+                 message, separator) -> str:
     """ returns the log message obfuscated"""
     patterns, repl = assist_filter(fields, redaction, separator)
     for i in range(len(fields)):
