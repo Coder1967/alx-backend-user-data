dd226a11b453fd8274acfd102d3653e53de5bc9b
Nosa <nosakhareaghedo42@gmail.com>
2023-03-08 13:37:37 +0000

    finishing up

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+implements session auth
+"""
+from models.user import User
+from typing import TypeVar
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    pass
